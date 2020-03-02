33ff0b7fa2736785705f686cdffb9ec21206a3f4
gomathi03 <61311018+gomathi03@users.noreply.github.com>
2020-03-02 14:41:15 +0530

    Create ScannerInteger.java

---
+++ ScannerInteger.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class ScannerInteger
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int num=s.nextInt();
+System.out.println("Enter the number");
+System.out.println("Integer i get from the user= "+num);
+}
+} 
+   
